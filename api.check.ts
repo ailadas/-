fbc0e6a759647a31122358765fa8fc7dcb0a2005
ailadas <131986114+ailadas@users.noreply.github.com>
2023-06-17 22:15:53 +0700

    Create api.check.ts

---
+++ api.check.ts
@@ -0,0 +1,7 @@
+Test and verify
+your checks
+ทดสอบทรัพยากรของคุณจากเครื่องภายในเครื่องของคุณหรือระหว่างการปรับใช้ใน CI ทั้งหมดทำงานบนโครงสร้างพื้นฐานส่วนกลางของ ChecklyDeploy to Checkly for
+
+24/7 monitoring
+
+Deploy, lifecycle, and scale your end-to-end testing and monitoring solution from the command line alongside your application deployments.
